2a39dc7292df7ecd1ffc274f41b1baf9fcf5a26f
LeZam <adamzamouricontact@gmaial.com>
2025-09-03 20:13:08 +0200

    feat: enhance drag functionality and animation in Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,9 @@
-import { motion } from "motion/react";
+import { animate, motion, useMotionValue } from "motion/react";
 import { useRef } from "react";
 
 const Home = () => {
   const countraintOverlayRef = useRef<HTMLDivElement>(null);
+  const y = useMotionValue(0);
 
   return (
     <div className="mt-[30%] h-full">
@@ -19,14 +20,37 @@ const Home = () => {
       </section>
       <div
         ref={countraintOverlayRef}
-        className="absolute top-10 h-[200vh] w-full"
+        className="absolute top-10 h-[200vh] w-full flex flex-col justify-end"
+        style={{ height: "calc(-161px + 200vh" }}
       >
         <motion.section
           drag="y"
+          dragTransition={{
+            power: 0.5,
+            bounceDamping: 20,
+          }}
           dragConstraints={countraintOverlayRef}
           dragMomentum={false}
           dragElastic={0}
-          className="relative h-screen w-full translate-y-3/4 rounded-t-4xl bg-gray-900 px-6 py-4 pt-8"
+          style={{ y }}
+          className="relative bottom-0 h-screen w-full rounded-t-4xl bg-gray-900 px-6 py-4 pt-8"
+          onDragEnd={(e, info) => {
+            let target: number = 0;
+            if (
+              info.point.y < window.innerHeight / 2 ||
+              info.velocity.y < -400
+            ) {
+              target =
+                -countraintOverlayRef.current!.offsetHeight +
+                window.innerHeight;
+            } else if (
+              info.point.y > window.innerHeight / 2 ||
+              info.velocity.y > 400
+            ) {
+              target = 0;
+            }
+            animate(y, target, { type: "spring", stiffness: 300, damping: 30 });
+          }}
         >
           <div className="absolute top-2 left-1/2 h-1 w-1/6 -translate-x-1/2 rounded-xl bg-gray-700"></div>
           <form className="w-full">
