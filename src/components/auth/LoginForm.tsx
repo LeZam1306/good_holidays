e16e189b65d47d7baba94ea9d17974453d4726ab
Lezam <adamzamouricontact@gmail.com>
2025-09-09 23:13:58 +0200

    feat: refactor LoginForm to remove Input component and implement inline input fields with validation

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,16 +1,52 @@
+import { useState, type FormEvent } from "react";
+import { useAuth } from "../../hooks/useAuth";
+import { credentialSchema } from "../../schemas/credential.schema";
+import type { credentialInterface } from "../../types/credential.interface";
 import Button from "./Button";
-import Input from "./Input";
 
 const LoginForm = () => {
-  const handleLogin = () => {};
+  const { mutate } = useAuth();
+  const [inputs, setInputs] = useState<credentialInterface>({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInputs((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  const handleLogin = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const result = credentialSchema.safeParse(inputs);
+    if (!result.success) {
+      console.log(result.error);
+    } else {
+      mutate({ credential: result.data, authType: "login" });
+    }
+  };
 
   return (
     <form
       onSubmit={handleLogin}
       className="mx-2 flex flex-col items-stretch gap-4"
     >
-      <Input name="email" type="mail" placeholder="myemail@mail.com" />
-      <Input name="password" type="password" placeholder="Password" />
+      <input
+        name="email"
+        type="email"
+        placeholder="myemail@mail.com"
+        className="w-full rounded-full bg-gray-900 px-4 py-3 text-lg text-white"
+        onChange={handleChange}
+      />
+      <input
+        name="password"
+        type="password"
+        placeholder="Password"
+        className="w-full rounded-full bg-gray-900 px-4 py-3 text-lg text-white"
+        onChange={handleChange}
+      />
       <Button type="submit">Login</Button>
       <button className="text-sm text-white underline" type="button">
         You forgot your password ? no problemo !
